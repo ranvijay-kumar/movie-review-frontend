cc7e2ea73591d4a03cabbd01b705c110ab11392e
ranvijay <mailkumarranvijay@gmail.com>
2024-03-15 20:05:07 +0530

    added movie component

---
+++ src/components/add-movie.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./add-movie.css";
+
+function AddMovie() {
+  return (
+    <div className="my-container p-4 d-flex flex-column justify-content-between">
+      <div className="">Add new movie</div>
+      <div className=""><input type="text"></input></div>
+      <div className=""><input type="date" placeholder="Release Date"></input></div>
+      <div className="d-flex flex-column align-items-end"><button type="button" className="btn add-review-btn">Create Movie</button></div>
+    </div>
+  );
+}
+
+export default AddMovie;
