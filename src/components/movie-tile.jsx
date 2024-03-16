3d404835d30fb8450d6590a4fd9f8f99940c862a
ranvijay <mailkumarranvijay@gmail.com>
2024-03-17 00:20:05 +0530

    updated movie tile

--- src/components/movie-tile.jsx
+++ src/components/movie-tile.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import './movie-tile.css'
+import './movie-tile.css';
 
 function MovieTile(props) {
   let name = props.movie.name;
-  let releaseDate = props.movie.releaseDate;
+  let releaseDate = new Date(props.movie.releaseDate).toLocaleDateString();
   let rating = props.movie.averageRating;
   let ratingStr = "";
+  if(rating) {
+    ratingStr = rating + "/10";
+  } else{
+    ratingStr = "Not Rated Yet"
+  }
   return (
       <div className="tile m-4 p-4 d-flex flex-column justify-content-around">
         <div className="px-2">{name}</div>
         <div className="px-2">Released: {releaseDate}</div>
-        <div className="px-2">Rating: {rating}/10</div>
+        <div className="px-2">Rating: {ratingStr}</div>
       </div>
   );
 }
